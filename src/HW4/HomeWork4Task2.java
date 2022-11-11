586fd0e89994e88400f83c6aec44632b961b46cb
Wirral_Riddler <anestshchuch@gmail.com>
2022-11-12 01:55:02 +0300

    done

--- src/HW4/HomeWork4Task2.java
+++ src/HW4/HomeWork4Task2.java
@@ -5,26 +5,36 @@ package HW4;
 
 import java.util.LinkedList;
 import java.util.logging.Logger;
+import java.util.Queue;
+import java.util.Deque; // сделала через методы deque, queue делает то же, только в более "ручном" режиме, как я поняла
 
-public class Queue {
+public class HomeWork4Task2 {
+    public static Deque<Integer> queueFirst = new LinkedList<Integer>();
     public static void main(String[] args) {
-        LinkedList<Integer> linkedTwo = new LinkedList<>(5);
+        //Queue<Integer> queueFirst = new LinkedList<Integer>();
 
         Logger log3 = Logger.getAnonymousLogger();
-        log3.info();
+        log3.info(String.valueOf(enqueue(6)));
 
-        enqueue();
-        dequeue();
-        first();
-    }
+        Logger log4 = Logger.getAnonymousLogger();
+        log4.info(String.valueOf(dequeue()));
 
-    public static int enqueue() {
-
-    }
-    public static int dequeue() {
+        Logger log5 = Logger.getAnonymousLogger();
+        log5.info(String.valueOf(first()));
 
     }
-    public static int first() {
 
+    public static Deque<Integer> enqueue(int args) { // помещает элемент в конец очереди
+        queueFirst.addLast(args);
+        return queueFirst;
+    }
+
+    public static Deque<Integer> dequeue() { // возвращает первый элемент из очереди и удаляет его
+        queueFirst.pollFirst();
+        return queueFirst;
+    }
+    public static Deque<Integer> first() { // возвращает первый элемент из очереди, не удаляя его
+        queueFirst.peekFirst();
+        return queueFirst;
     }
 }

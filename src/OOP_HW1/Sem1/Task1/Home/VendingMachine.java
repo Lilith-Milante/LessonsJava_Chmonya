3b665871f7bfa5c1aab07336f9de68a1509ad5d6
Lilith-Milante <106370390+Lilith-Milante@users.noreply.github.com>
2022-11-25 12:59:22 +0300

    Update VendingMachine.java

--- src/OOP_HW1/Sem1/Task1/Home/VendingMachine.java
+++ src/OOP_HW1/Sem1/Task1/Home/VendingMachine.java
@@ -2,6 +2,6 @@ package OOP_HW1.Sem1.Task1.Home;
 
 public interface VendingMachine {
 
-    public HotDrinkHeir getProduct(String name, double volume, int temperature);
+    public HotDrinkHeir getProduct(String name, Double volume, int temperature);
 
 }

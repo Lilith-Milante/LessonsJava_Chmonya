7b6ff2d1b620ec614734230b289211ea581344e2
Wirral_Riddler <anestshchuch@gmail.com>
2022-12-07 00:51:17 +0300

    done

---
+++ src/OOP_Homework/Planner/Main/Main.java
@@ -0,0 +1,9 @@
+package OOP_Homework.Planner.Main;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Controller controller = new Controller();
+        controller.controller();
+    }
+}
